9f9e3471159e5bde9ae4690670f3a8de9ecc1c25
Divyansh Agrawal <91237856+divyansh89532@users.noreply.github.com>
2023-08-10 12:02:46 +0530

    Create Search_in_Rotated_Sorted_Array_II.java

---
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int num:nums)
+        {
+            if (target==num)
+            return true;
+        }
+        return false;
+    }
+}
